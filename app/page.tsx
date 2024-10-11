ab65d761f8059791259dc7fcb5b77063468e37bb
Naman-Jaju <namanjaju2004@gmail.com>
2024-10-11 14:52:00 +0530

    scoping data to user identifier

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,23 @@
 "use client"
+import { api } from "@/convex/_generated/api";
 import { SignInButton, UserButton } from "@clerk/clerk-react";
-import { Authenticated, Unauthenticated } from "convex/react";
+import { Authenticated, Unauthenticated, useMutation, useQuery } from "convex/react";
 
 export default function Home() {
+
+  const documents = useQuery(api.documents.getDocuments,{});
+  const creatDocument = useMutation(api.documents.createDocuments);
+
   return (
-    <main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Unauthenticated>
         <SignInButton />
       </Unauthenticated>
       <Authenticated>
         <UserButton />
+
+        <button onClick={() => creatDocument({title: "test"}) }>Click Me</button>
+        {documents?.map((doc) => <div key={doc._id}>{doc.title}</div>)} 
       </Authenticated>
     </main>
   );
